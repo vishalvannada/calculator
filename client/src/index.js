055206aa49ad0f0f2a118959279d59fbecd0d795
vishalvannada <vannadavishal@gmail.com>
2018-02-27 02:33:33 -0800

    Adding Changes Finally, Latest Version

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import swal from 'sweetalert';
 
 class App extends Component {
 
@@ -16,6 +15,22 @@ class App extends Component {
     }
 
     addition() {
+
+        if (isNaN(this.state.first) || isNaN(this.state.second)) {
+            swal("Please Enter a Number");
+            return;
+        }
+
+        if (this.state.first === "") {
+            swal("Please enter the first number");
+            return;
+        }
+
+        if (this.state.second === "") {
+            swal("Please enter the second number");
+            return;
+        }
+
         console.log("here");
         axios.post('http://localhost:3001/add', {
             first: this.state.first,
@@ -32,7 +47,20 @@ class App extends Component {
     }
 
     subtraction() {
-        console.log("here");
+        if (isNaN(this.state.first) || isNaN(this.state.second)) {
+            swal("Please Enter a Number");
+            return;
+        }
+
+        if (this.state.first === "") {
+            swal("Please enter the first number");
+            return;
+        }
+
+        if (this.state.second === "") {
+            swal("Please enter the second number");
+            return;
+        }
 
         axios.post('http://localhost:3001/subtract', {
             first: this.state.first,
@@ -49,6 +77,20 @@ class App extends Component {
     }
 
     multiplication() {
+        if (isNaN(this.state.first) || isNaN(this.state.second)) {
+            swal("Please Enter a Number");
+            return;
+        }
+
+        if (this.state.first === "") {
+            swal("Please enter the first number");
+            return;
+        }
+
+        if (this.state.second === "") {
+            swal("Please enter the second number");
+            return;
+        }
         axios.post('http://localhost:3001/multiply', {
             first: this.state.first,
             second: this.state.second
@@ -64,6 +106,22 @@ class App extends Component {
     }
 
     division() {
+        
+        if (isNaN(this.state.first) || isNaN(this.state.second)) {
+            swal("Please Enter a Number");
+            return;
+        }
+
+        if (this.state.first === "") {
+            swal("Please enter the first number");
+            return;
+        }
+
+        if (this.state.second === "") {
+            swal("Please enter the second number");
+            return;
+        }
+
         axios.post('http://localhost:3001/divide', {
             first: this.state.first,
             second: this.state.second
@@ -81,68 +139,60 @@ class App extends Component {
 
     render() {
 
-        const style = {
-            margin: 12,
-        };
-
-        const labelStyle = {
-            fontWeight: 'bold',
-        };
+        console.log("Vis");
 
         const isEmpty = () => {
-            console.log(this.state.answer === '');
             return this.state.answer === '';
         };
 
         return (
             <div className="container">
                 <div className="jumbotron mt-5">
-                    <h1 className="text-center">Calculator</h1>
+                    <h3 className="text-center">Calculator</h3>
 
                     <div className="form-group col-md-6 mt-4 mx-auto">
                         <input
                             value={this.state.first}
-                            name="first" className="form-control" type="number" step="any"
+                            name="first" className="form-control" type="number" step="1"
                             id="first" placeholder="First Number"
-                            onChange={event => this.setState({first: event.target.value})}
+                            onChange={event => this.setState({first: event.target.value})
+                            }
                         />
                     </div>
 
                     <div className="form-group col-md-6 mx-auto">
                         <input
                             value={this.state.second}
-                            className="form-control" name="second" type="number" step="any"
+                            className="form-control" name="second" type="number" step="1"
                             id="second" placeholder="Second Number"
-                            onChange={event => this.setState({second: event.target.value})}
+                            onChange={
+                                event => this.setState({second: event.target.value})}
                         />
                     </div>
 
+                    <div className="text-center mt-5">
+                        <button
+                            onClick={() => this.addition()} type="button" className="btn btn-info mr-4">Addition
+                        </button>
+                        <button
+                            onClick={() => this.subtraction()} type="button" className="btn btn-info mr-4">Subtraction
+                        </button>
+                        <button
+                            onClick={() => this.multiplication()} type="button" className="btn btn-info mr-4">Multiplication
+                        </button>
+                        <button
+                            onClick={() => this.division()} type="button" className="btn btn-info mr-4">Division
+                        </button>
 
-                    <MuiThemeProvider>
-                        <RaisedButton onClick={() => this.addition()} label="Addition" labelColor="#FAFAFA"
-                                      backgroundColor="#BF360C" labelStyle={labelStyle} style={style}/>
-                    </MuiThemeProvider>
-                    <MuiThemeProvider>
-                        <RaisedButton onClick={() => this.subtraction()} label="Subtraction" labelColor="#FAFAFA"
-                                      backgroundColor="#BF360C" labelStyle={labelStyle} style={style}/>
-                    </MuiThemeProvider>
-                    <MuiThemeProvider>
-                        <RaisedButton onClick={() => this.multiplication()} label="Multiplication" labelColor="#FAFAFA"
-                                      backgroundColor="#BF360C" labelStyle={labelStyle}
-                                      style={style}/>
-                    </MuiThemeProvider>
-                    <MuiThemeProvider>
-                        <RaisedButton onClick={() => this.division()} label="Division" labelColor="#FAFAFA"
-                                      backgroundColor="#BF360C" labelStyle={labelStyle} style={style}/>
-                    </MuiThemeProvider>
+                    </div>
 
-                    <h3 className={isEmpty() ? "class1" : "class2"}>Result is {this.state.answer}</h3>
+
+                    <h3 className={isEmpty() ? "class1 mt-4" : "class2 mt-4"}>Result is {this.state.answer}</h3>
 
 
                 </div>
             </div>
-        )
-            ;
+        );
     }
 }
 
